e9fb41c471ebbe4f45ff64c72978be6614ba68e3
Jens Andersson <jens@fritan.com>
2013-06-24 08:46:06 +0200

    Revert "Fixed issue with starting in landscape on ipad"
    This reverts commit 107469273a7ff48e905b045246b4972e2ce7815f.

--- ViewDeck/IIViewDeckController.m
+++ ViewDeck/IIViewDeckController.m
@@ -492,12 +492,9 @@ static NSTimeInterval durationToAnimate(CGFloat pointsToAnimate, CGFloat velocit
 }
 
 - (CGRect)referenceBounds {
-    CGRect bounds = [[UIScreen mainScreen] bounds]; // portrait bounds
-    if (UIInterfaceOrientationIsLandscape([[UIApplication sharedApplication] statusBarOrientation])) {
-        bounds.size = CGSizeMake(bounds.size.height, bounds.size.width);
-    }
-    return bounds;
-    // return self.referenceView ? self.referenceView.bounds : [[UIScreen mainScreen] bounds];
+    return self.referenceView
+        ? self.referenceView.bounds
+        : [[UIScreen mainScreen] bounds];
 }
 
 - (CGFloat)relativeStatusBarHeight {
